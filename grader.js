7d828431dbcba69ce2eedba30ddd920648a085fd
Deniss Samcuks <deniss.samcuks@gmail.com>
2013-07-10 01:27:39 +0300

    URL optoin support is added to grader

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -44,8 +45,8 @@ var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtmlFile = function(html, checksfile) {
+    $ = html;
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -61,14 +62,28 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var performChecksAndOutput = function(html) {
+    var checkJson = checkHtmlFile(html, program.checks);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+}
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'URL to web resource')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+    if (program.url != undefined) {
+        rest.get(program.url).on('complete', function(data) {
+            var html = cheerio.load(data);
+            performChecksAndOutput(html);
+        });
+    } else {
+        var html = cheerioHtmlFile(program.file);
+        performChecksAndOutput(html);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
